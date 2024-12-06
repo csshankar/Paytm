6d829761c58aed5af452a943bdec2425646b6b75
Rex <your_username@users.noreply.github.com>
2024-12-07 00:37:36 +0530

    build-fix

--- apps/user-app/next.config.js
+++ apps/user-app/next.config.js
@@ -1,16 +1,7 @@
-const { PrismaPlugin } = require('@prisma/nextjs-monorepo-workaround-plugin')
-
 /** @type {import('next').NextConfig} */
 module.exports = {
   transpilePackages: ["@repo/ui"],
-  output: 'standalone',
-  webpack: (config, { isServer }) => {
-    if (isServer) {
-      config.plugins = [...config.plugins, new PrismaPlugin()]
-    }
-
-    return config
-  },
+  output: 'standalone'
 };
 
 
